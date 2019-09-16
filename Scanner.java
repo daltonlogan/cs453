ca1ebe6fd6dcacf36ed3527a87e39ff091cc881d
Eric Thomas <erict3742@email.arizona.edu>
2019-09-15 18:53:32 -0700

    Add first iteration of extractToken
    This is the initial code that will extract a single
    token from a string.

--- Scanner.java
+++ Scanner.java
@@ -1,26 +1,86 @@
-public class Scanner{
-  enum TokenType{
-      NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE;
-  }
-
-  class Token{
-    TokenType tokenType;
-    String tokenVal;
-    public Token(TokenType tokenType, String tokenVal){
-      this.tokenType = tokenType;
-      this.tokenVal = tokenVal;
+public class Scanner
+{
+    enum TokenType
+    {
+        NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE, INVALID
     }
-    public String toString(){
-      return this.tokenType + ": " + this.tokenVal + " ";
+
+    class Token
+    {
+        TokenType tokenType;
+        String tokenVal;
+
+        public Token( TokenType tokenType, String tokenVal )
+        {
+            this.tokenType = tokenType;
+            this.tokenVal = tokenVal;
+        }
+
+        public String toString( )
+        {
+            return this.tokenType + ": " + this.tokenVal + " ";
+        }
     }
-  }
 
-  public Token extractToken(StringBuilder stream){
-    /* TODO #2: Extract the next token in the string, or report an error*/
-    return null;
-  }
+    public Token extractToken( StringBuilder stream )
+    {
+        char aChar = stream.charAt( 0 );
+        TokenType aType;
 
-  public String extractTokens(String arg){
+        if ( Character.isDigit( aChar ) )
+        {
+            aType = TokenType.NUM;
+        }
+        else if ( aChar == '+' )
+        {
+            aType = TokenType.PLUS;
+        }
+        else if ( aChar == '-' )
+        {
+            aType = TokenType.MINUS;
+        }
+        else if ( aChar == '*' )
+        {
+            aType = TokenType.MUL;
+        }
+        else if ( aChar == '/' )
+        {
+            aType = TokenType.DIV;
+        }
+        else if ( aChar == '<' )
+        {
+            if ( stream.charAt( 1 ) == '=' )
+            {
+                aType = TokenType.LTE;
+            }
+            else
+            {
+                aType = TokenType.LT;
+            }
+        }
+        else if ( aChar == '>' )
+        {
+            if ( stream.charAt( 1 ) == '=' )
+            {
+                aType = TokenType.GTE;
+            }
+            else
+            {
+                aType = TokenType.GT;
+            }
+        }
+        else
+        {
+            aType = TokenType.INVALID;
+        }
+
+        String aValue = String.valueOf( aChar );
+        Token aToken = new Token( aType, aValue );
+        return aToken;
+    }
+
+    public String extractTokens( String arg )
+    {
     /* TODO #1: Finish this function to iterate over all tokens in the input string.
 
        Pseudo code:
@@ -31,7 +91,7 @@ public class Scanner{
             result += nextToken.toString()
          return result
     */
-    return null;
-  }
+        return null;
+    }
 
 }
