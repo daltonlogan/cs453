adfb2f31d156c0c92f2f96453592e456eb5a3664
Eric Thomas <bad.llama.group@gmail.com>
2019-09-19 23:06:26 -0700

    Add functionality to parser
    Initial implementation of parser and tests.

--- EvalParser.java
+++ EvalParser.java
@@ -1,8 +1,9 @@
-import java.util.*;
+
 import java.lang.String;
 
 public class EvalParser {
-  Scanner scan;
+  Scanner scan = new Scanner();
+  String evalString;
 
   int tempID = 0;
   String threeAddressResult = "";
@@ -14,17 +15,109 @@ public class EvalParser {
 
   /***************** Simple Expression Evaluator ***********************/
   // TODO #1 Continued: Write the functions for E/E', T/T', and F. Return the expression's value
+
+  private int E()
+  {
+    int result = T();
+    while( true )
+    {
+      Scanner.Token nextToken = lookahead();
+      if( nextToken != null && nextToken.tokenType == Scanner.TokenType.PLUS )
+      {
+        match( nextToken );
+        result += T();
+        continue;
+      }
+      else if( nextToken != null && nextToken.tokenType == Scanner.TokenType.MINUS )
+      {
+        match( nextToken );
+        result -= T();
+        continue;
+      }
+      return result;
+    }
+  }
+
+  private int T()
+  {
+    int result = F();
+    while ( true )
+    {
+      Scanner.Token nextToken = lookahead();
+      if( nextToken != null && nextToken.tokenType == Scanner.TokenType.MUL )
+      {
+        match( nextToken );
+        result *= F();
+        continue;
+      }
+      else if( nextToken != null && nextToken.tokenType == Scanner.TokenType.DIV )
+      {
+        match( nextToken );
+        result /= F();
+        continue;
+      }
+      return result;
+    }
+  }
+
+  private int F()
+  {
+    Scanner.Token nextToken = lookahead();
+    int result = 0;
+    if( nextToken != null && nextToken.tokenType == Scanner.TokenType.LEFTPAREN )
+    {
+      match( nextToken );
+      nextToken = lookahead();
+      while( nextToken != null && nextToken.tokenType != Scanner.TokenType.RIGHTPAREN )
+      {
+        result += E();
+        nextToken = lookahead();
+      }
+      match( nextToken );
+    }
+    else if( nextToken != null && nextToken.tokenType == Scanner.TokenType.NUM )
+    {
+      int num = Integer.parseInt( nextToken.tokenVal );
+      match( nextToken );
+      return num;
+    }
+    return result;
+  }
+
   /****************************************/
 
   /* TODO #1: Write a parser that can evaluate expressions */
   public int evaluateExpression(String eval){
-    return 0;
+    evalString = eval;
+    return E();
   }
 
   /* TODO #2: Now add three address translation to your parser*/
   public String getThreeAddr(String eval){
     this.threeAddressResult = "";
     return this.threeAddressResult;
-  } 
+  }
+
+  private Scanner.Token lookahead()
+  {
+    if( evalString.isEmpty() )
+    {
+      return null;
+    }
+    return scan.extractToken(new StringBuilder(evalString));
+  }
+
+  private void match(Scanner.Token aToken)
+  {
+    if( aToken == null )
+    {
+      return;
+    }
+    while(Character.isWhitespace( evalString.charAt(0) ))
+    {
+      evalString = evalString.substring(1);
+    }
+    evalString = evalString.substring(aToken.tokenVal.length());
+  }
 
 }
--- EvalParserTest.java
+++ EvalParserTest.java
@@ -3,20 +3,65 @@ public class EvalParserTest{
   public static void TestExpressionEval(){
     System.out.println("*******************************************");
     System.out.println("Testing Expression evaluation");
+    System.out.println("*******************************************");
     EvalParser parser = new EvalParser();
 
+    System.out.println( "Starting test 1" );
     String eval = "9+(2*2)";
-    assert(parser.evaluateExpression(eval) == 13);
+    if ((parser.evaluateExpression(eval) != 13)) throw new AssertionError();
+    System.out.println( "Passed test 1" );
 
+    System.out.println();
+
+    System.out.println( "Starting test 2" );
     eval = "10-9";
-    assert(parser.evaluateExpression(eval) == 1);
+    if ((parser.evaluateExpression(eval) != 1)) throw new AssertionError();
+    System.out.println( "Passed test 2" );
 
+    System.out.println();
+
+    System.out.println( "Starting test 3" );
     eval = "3-5*17";
-    assert(parser.evaluateExpression(eval) == -82);
+    if ((parser.evaluateExpression(eval) != -82)) throw new AssertionError();
+    System.out.println( "Passed test 3" );
+
+    System.out.println();
 
     System.out.println("Congrats: expression evaluation tests passed! Now make your own test cases "+
                        "(this is only a subset of what we will test your code on)");
     System.out.println("*******************************************");
+
+    System.out.println( "*******************************************" );
+    System.out.println( "Testing Custom Expression Evaluation" );
+    System.out.println( "*******************************************" );
+
+    System.out.println( "Starting test 1" );
+    eval = "3*2*2";
+    if ((parser.evaluateExpression(eval) != 12)) throw new AssertionError();
+    System.out.println( "Passed test 1" );
+
+    System.out.println();
+
+    System.out.println( "Starting test 2" );
+    eval = "(2 - 3 * 2) + 2";
+    int a = parser.evaluateExpression(eval);
+    if ((parser.evaluateExpression(eval) != -2)) throw new AssertionError();
+    System.out.println( "Passed test 2" );
+
+    System.out.println();
+
+    System.out.println( "Starting test 3" );
+    eval = "(2) + (2)";
+    if ((parser.evaluateExpression(eval) != 4)) throw new AssertionError();
+    System.out.println( "Passed test 3" );
+
+    System.out.println();
+
+    System.out.println( "*******************************************" );
+    System.out.println( "Passed All Tests" );
+    System.out.println( "*******************************************" );
+
+
   }
 
   public static void TestThreeAddrGen(){
