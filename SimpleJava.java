8bbe742faffe871d5c210a9bfc040a6063efb156
Eric Thomas <bad.llama.group@gmail.com>
2019-10-03 12:10:34 -0700

    Add assignment 3 files
    This adds the skeleton code for assignment 3

---
+++ SimpleJava.java
@@ -0,0 +1,17 @@
+public class SimpleJava {
+
+    Scanner scan;
+    int tempID = 0;
+    int tlabelID = 0; // Label id for true
+    int flabelID = 0; // Label id for false
+    int rlabelID = 0; // Label id for loops
+
+    public String getThreeAddr(String eval){
+        return "";
+    }
+
+    public static void main(String[] args){
+        SimpleJava parser = new SimpleJava();
+    }
+
+}
