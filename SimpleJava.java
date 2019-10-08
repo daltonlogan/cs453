3435588335c97ecd7aa0ea6bbdaacdca756dddb0
Eric Thomas <bad.llama.group@gmail.com>
2019-10-08 01:00:30 -0700

    Remove debugging string

--- SimpleJava.java
+++ SimpleJava.java
@@ -3,7 +3,6 @@ public class SimpleJava {
     public String getThreeAddr(String eval){
         EvalParser parser = new EvalParser();
         node root = parser.program( eval );
-        String a = parser.emitTAC( root );
-        return a;
+        return parser.emitTAC( root );
     }
 }
