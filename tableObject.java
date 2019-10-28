bd6088e1adb0c12738baccdf8d1503ff75dceb02
Dalton Logan <daltonlogan@email.arizona.edu>
2019-10-27 20:50:36 -0700

    Add files via upload
    Fixed symbolTable to actually work, tableObject to be it's own class and have a constructor, and added a seperate enum for Operator to be used across the classes.

---
+++ tableObject.java
@@ -0,0 +1,10 @@
+
+
+public class tableObject{
+	
+	tableObject(String iden, Operation typeNew, String val) {//placeholder object, value should be something other than a string. Possibly an operand?
+		String identifier = iden;
+		Operation type = typeNew; 
+		String value = val;
+	}
+}
