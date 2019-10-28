ea89cf808eac8d60b5288e9c2c298bd03d5e4ecc
Dalton Logan <daltonlogan@email.arizona.edu>
2019-10-27 21:19:27 -0700

    Add files via upload
    Added identifier and value to Symbol, also added getters/setters for remaining variables.

--- Symbol.java
+++ Symbol.java
@@ -5,9 +5,36 @@ public class Symbol
         LEXEME, ID, INT, FUNC
     }
     private SymbolType type;
-
+    private String identifier;
+    private String value;
+    
     public void setType( SymbolType type )
     {
         this.type = type;
+    }
+    
+    public void setIdentifier( String identifier )
+    {
+    	this.identifier = identifier;
+    }
+    
+    public void setValue( String value )
+    {
+    	this.value = value;
+    }
+    
+    public SymbolType getType()
+    {
+    	return this.type;
+    }
+    
+    public String getIdentifier()
+    {
+    	return this.identifier;
+    }
+    
+    public String getValue()
+    {
+    	return this.value;
     }
 }
